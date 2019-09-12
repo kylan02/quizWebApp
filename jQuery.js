8e3491ab12207558e19b07fe9f92a3fec20f7637
Kylan O'Connor <kylan02@gmail.com>
2019-09-12 10:15:39 -0700

    make quiz better

--- jQuery.js
+++ jQuery.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-	$(".question, .answer").hide();
+	$(".question, .answer, .picture, #finish").hide();
 	x=0
-	$("#q"+x).click(function(){
+	$(".next").click(function(){
 		$("#a"+x).show();
+		$("#p"+x).fadeIn();
 		x=x+1
 		$("#q"+x).show();
+		$("#div"+(x-1)).css({"border": "2px solid grey", "margin": "10px", "border-radius": "30px"});
+		if(x==5){
+			$("#next").hide();
+			$("#finish").show();
+		}
 	});
 });
