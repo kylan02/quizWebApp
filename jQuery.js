040913a10afbbb3f85e8e25c824861ae5700fd47
Kylan O'Connor <kylan02@gmail.com>
2019-09-13 09:28:05 -0700

    final changes

--- jQuery.js
+++ jQuery.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
 		$("#p"+x).fadeIn();
 		x=x+1
 		$("#q"+x).show();
-		$("#div"+(x-1)).css({"border": "2px solid grey", "margin": "10px", "border-radius": "30px"});
+		$("#div"+(x-1)).css({"border": "2px solid grey", "margin": "10px", "border-radius": "30px", "margin-left": "100px", "margin-right": "100px", "padding": "5px"});
+		$(document).scrollTop($(document).height());
 		if(x==5){
 			$("#next").hide();
 			$("#finish").show();
